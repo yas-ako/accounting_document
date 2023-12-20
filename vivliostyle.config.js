69f9d52181abb80aa27b5ba8184ca62bb6a1ebfd
yasako <105139975+yas-ako@users.noreply.github.com>
2023-12-20 11:22:55 +0900

    update: ファイルを生成するディレクトリの設定

--- vivliostyle.config.js
+++ vivliostyle.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  title: "会計班引継ぎ資料",
+  theme: "css/style.css",
+  language: "ja",
+  author: "第75期記念祭幹部",
   entry: ["0_frontmatter.md"],
-  output: ["output.pdf"],
+  output: ".vivliostyle/output.pdf",
+  workspaceDir: ".vivliostyle",
 };
